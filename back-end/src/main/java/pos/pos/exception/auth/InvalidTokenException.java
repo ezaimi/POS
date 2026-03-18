020e8c22a3c25195b1a747cd7e0bc4e97020de9d
Erisa Zaimi <ezaim21@epoka.edu.al>
2026-03-18 21:27:40 +0100

    Adding Invalid Token exeption

---
+++ back-end/src/main/java/pos/pos/exception/auth/InvalidTokenException.java
@@ -0,0 +1,12 @@
+package pos.pos.exception.auth;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.UNAUTHORIZED)
+public class InvalidTokenException extends RuntimeException {
+
+    public InvalidTokenException() {
+        super("Invalid token");
+    }
+}
