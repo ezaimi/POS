2c37877ce43c32924a69212cdddf0b49a76beafe
David <dkeci21@epoka.edu.al>
2026-03-19 20:08:35 +0100

    upadting tests so it work with postgress

--- back-end/src/test/java/pos/pos/user/repository/UserRepositoryTest.java
+++ back-end/src/test/java/pos/pos/user/repository/UserRepositoryTest.java
@@ -3,7 +3,9 @@ package pos.pos.user.repository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
 import pos.pos.user.entity.User;
 
 import java.time.OffsetDateTime;
@@ -12,6 +14,8 @@ import java.util.Optional;
 import static org.junit.jupiter.api.Assertions.*;
 
 @DataJpaTest
+@ActiveProfiles("test")
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class UserRepositoryTest {
 
     @Autowired
