f4515f7a1e407c4d3ab045c9a125014c28885095
David <dkeci21@epoka.edu.al>
2026-03-21 20:36:51 +0100

    Adding bootstrap configuration so we can create super admin

--- back-end/src/main/java/pos/pos/PosApplication.java
+++ back-end/src/main/java/pos/pos/PosApplication.java
@@ -2,8 +2,11 @@ package pos.pos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import pos.pos.config.properties.BootstrapSuperAdminProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(BootstrapSuperAdminProperties.class)
 public class PosApplication {
 
 	public static void main(String[] args) {
