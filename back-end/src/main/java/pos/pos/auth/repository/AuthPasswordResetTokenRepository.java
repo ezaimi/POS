4b139309c15e20605e763daaad7b1feae17ca5a0
Erisa Zaimi <zaimi.erisa2@gmail.com>
2026-04-04 19:24:29 +0200

    Add password reset service and repository queries (v1)

--- back-end/src/main/java/pos/pos/auth/repository/AuthPasswordResetTokenRepository.java
+++ back-end/src/main/java/pos/pos/auth/repository/AuthPasswordResetTokenRepository.java
@@ -6,9 +6,20 @@ import org.springframework.data.jpa.repository.Query;
 import pos.pos.auth.entity.AuthPasswordResetToken;
 
 import java.time.OffsetDateTime;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface AuthPasswordResetTokenRepository extends JpaRepository<AuthPasswordResetToken, UUID> {
+
+    Optional<AuthPasswordResetToken> findByTokenHashAndUsedAtIsNullAndExpiresAtAfter(
+            String tokenHash,
+            OffsetDateTime now
+    );
+
+    boolean existsByUserIdAndCreatedAtAfter(UUID userId, OffsetDateTime cutoff);
+
+    @Modifying
+    void deleteByUserId(UUID userId);
 
     @Modifying
     @Query("""
